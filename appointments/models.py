e42c6c2cde08f5328a18f2adf00dcfec80d80994
Sanjid Shahriar Sajin <sanjidshahriar07@gmail.com>
2025-10-21 22:48:01 +0600

    Refactor Doctor model and update related classes

--- appointments/models.py
+++ appointments/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Doctor(models.Model):
-<<<<<<< HEAD
-    image = models.ImageField()
-=======
     image = models.ImageField(upload_to='doctors/', null=True, blank=True)
->>>>>>> 95193974668127615266de434effc1643d28f090
     name = models.CharField(max_length=255)
     specialty = models.CharField(max_length=255)
     status = models.BooleanField(default=True)
@@ -17,36 +14,29 @@ class Doctor(models.Model):
     def __str__(self):
         return self.name
 
+
 class DoctorTimeSlot(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     start_time = models.TimeField()
     end_time = models.TimeField()
-<<<<<<< HEAD
-
-    def __str__(self):
-        return f"{self.doctor.name} ({self.start_time} - {self.end_time})"
-=======
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.doctor.name} - {self.start_time} to {self.end_time}"
->>>>>>> 95193974668127615266de434effc1643d28f090
+        return f"{self.doctor.name} ({self.start_time} - {self.end_time})"
+
 
 class Appointment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-<<<<<<< HEAD
     doctor_time_slot = models.ForeignKey(DoctorTimeSlot, on_delete=models.CASCADE)
-=======
->>>>>>> 95193974668127615266de434effc1643d28f090
     description = models.CharField(max_length=1000)
     appointment_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     serial_number = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-<<<<<<< HEAD
-        return self.user.username
+        return f"{self.user.username} - {self.doctor.name}"
+
 
 class Hospital(models.Model):
     hospital_name = models.CharField(max_length=100)
@@ -56,6 +46,7 @@ class Hospital(models.Model):
 
     def __str__(self):
         return self.hospital_name
+
 
 class Blood(models.Model):
     BLOOD_GROUP_CHOICES = (
@@ -68,6 +59,3 @@ class Blood(models.Model):
 
     def __str__(self):
         return f"{self.blood_group}"
-=======
-        return f"{self.user.username} - {self.doctor.name}"
->>>>>>> 95193974668127615266de434effc1643d28f090
